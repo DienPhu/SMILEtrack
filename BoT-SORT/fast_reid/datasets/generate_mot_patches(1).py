4afe8f504310725e9b8e1406ffe05c0820151571
DienPhu <145919100+DienPhu@users.noreply.github.com>
2023-10-27 14:11:27 +0700

    Update generate_mot_patches(1).py

--- BoT-SORT/fast_reid/datasets/generate_mot_patches(1).py
+++ BoT-SORT/fast_reid/datasets/generate_mot_patches(1).py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def generate_trajectories_from_yolo(file_path):
+def generate_trajectories(file_path):
     with open(file_path, 'r') as f:
         lines = f.read().split('\n')
         values = []
